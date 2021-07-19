54a2714aba633bbaa9bcfb94a894675d32d4ae38
Axel Jorgensen <code@axeljorgensen.info>
2021-07-18 17:20:40 -0700

    stylization

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let deckID;
 
-function newDeck(e) {
+function newGame(e) {
     e.preventDefault();
     fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
         .then(res => res.json())
@@ -9,14 +9,23 @@ function newDeck(e) {
         })
 }
 
-document.getElementById('newDeckButton').addEventListener('click', newDeck);
+document.getElementById('newGameButton').addEventListener('click', newGame);
 
 function draw(e) {
 
     e.preventDefault();
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckID}/draw/?count=2`)
         .then(res => res.json())
-        .then(data => console.log(data.cards))
+        .then(data => {
+            const cardOne = data.cards[0];
+            const cardTwo = data.cards[1];
+            document.getElementById('cardOne').innerHTML = `
+                <img src="${cardOne.image}" alt="${cardOne.value} of ${cardOne.suit}" class="card" /> 
+            `
+            document.getElementById('cardTwo').innerHTML = `
+                <img src="${cardTwo.image}" alt="${cardTwo.value} of ${cardTwo.suit}" class="card" /> 
+            `
+        })
 }
 
 document.getElementById('drawButton').addEventListener('click', draw);
